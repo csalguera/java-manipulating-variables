092cfc3a7c452f8a37a1148200eec852bf2f251a
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-06 20:41:56 -0400

    fix typo

--- src/PlusAndMinus.java
+++ src/PlusAndMinus.java
@@ -1,6 +1,8 @@
 public class PlusAndMinus {
   public static void main(String[] args) {
-    int zebrazInZoo = 8;
+    int zebrasInZoo = 8;
     int giraffesInZoo = 4;
+    int animalsInZoo = zebrasInZoo + giraffesInZoo;
+    System.out.println(animalsInZoo);
   }
 }
