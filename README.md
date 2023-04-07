77e3fc8d4956699d1161d2b25a297408f6dfffca
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-06 20:27:39 -0400

    stub up GuessingGame exercise

---
+++ README.md
@@ -0,0 +1,19 @@
+## Java Manipulating Variables
+
+## Guessing Game
+
+```
+public class GuessingGame {
+	public static void main(String[] args) {   
+		int mystery1 = 8 + 6;
+    int mystery2 = 8 - 6;
+	}
+}
+```
+
+In the file GuessingGame.java, we have defined two integers mystery1 and mystery2.
+
+We will talk about these operators, among others, in the rest of the lesson.
+
+Use System.out.println() to print the variable that holds a value of 2.
+
--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    // Introduction ------------------------------------------------
+    // We can manipulate Java variables via the usage of expressions, arithmetic operators, and more
+
+    // Java has built-in arithmetic operations that perform calculations on numeric values:
+
+    // declare initial balance
+    double balance = 20000.99;
+    // declare deposit amount
+    double depositAmount = 1000.00;
+    // store result of calculation in our original variable
+    balance = balance + depositAmount;
+    System.out.println(balance);
+    // balance + depositAmount is an expression
+
+    // the data types of variables determine which operations can be performed to manipulate it
+
+    // the data type of an expression is determined by the resulting value
+
+    // an expression of two int values will evaluate to an int value
+
+    // an expression of two double values will evaluate to a double value
+  }
 }
