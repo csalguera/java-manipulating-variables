3e3c1fb2099e15f3fca8c55d999abd2b10a94b40
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-07 14:37:56 -0400

    solve Operations expression2

--- src/Operations.java
+++ src/Operations.java
@@ -10,9 +10,20 @@ public class Operations {
     // 1 - 7 = -6
 
     System.out.println(expression1);
-    
+
     int expression2 = (3 + (2 * 2 - 5)) + 6 - 5;
-    // System.out.println(expression2);
+
+    // (): 2 * 2 = 4
+    // (3 + (4 - 5)) + 6 - 5
+    // (): 4 - 5 = -1
+    // (3 + -1) + 6 - 5
+    // (): 3 + -1 || 3 - 1 = 2
+    // 2 + 6 - 5
+    // +: 2 + 6 = 8
+    // 8 - 5 = 3
+
+    System.out.println(expression2);
+    
     int expression3 = 5 * 4 % 3 - 2 + 1;
     // System.out.println(expression3);
   }
