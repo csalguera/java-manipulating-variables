69ca321f6f3ce2ae819bfccb48b36fb0c15683e1
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-07 15:54:13 -0400

    solve BankAccount exercise

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,6 +1,19 @@
+import javax.xml.transform.stream.StreamSource;
+
 public class BankAccount {
   public static void main(String[] args) {
     double balance = 1000.75;
     double amountToWithdraw = 250;
+
+    double updatedBalance = balance - amountToWithdraw;
+    System.out.println(updatedBalance);
+
+    double amountForEachFriend = updatedBalance / 3;
+    System.out.println(amountForEachFriend);
+
+    boolean canPurchaseTicket = amountForEachFriend >= 250;
+    System.out.println(canPurchaseTicket);
+
+    System.out.println("I gave each friend " + amountForEachFriend + "...");
   }
 }
