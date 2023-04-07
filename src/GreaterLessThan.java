785ecaa16696d342c2f327291970fce7d08d86d3
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-07 14:51:38 -0400

    solve GreaterLessThan exercise

--- src/GreaterLessThan.java
+++ src/GreaterLessThan.java
@@ -3,5 +3,11 @@ public class GreaterLessThan {
     double creditsEarned = 176.5;
     double creditsOfSeminar = 8;
     double creditsToGraduate = 180;
+
+    System.out.println(creditsEarned > creditsToGraduate);
+    
+    double creditsAfterSeminar = creditsOfSeminar + creditsEarned;
+
+    System.out.println(creditsToGraduate < creditsAfterSeminar);
   }
 }
