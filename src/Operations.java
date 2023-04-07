9073a6021d743df2e8fa25a86ad895b3189880b7
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-07 14:34:50 -0400

    solve Operations expression1

--- src/Operations.java
+++ src/Operations.java
@@ -1,7 +1,16 @@
 public class Operations {
   public static void main(String[] args) {
     int expression1 = 5 % 2 - (4 * 2 - 1);
-    // System.out.println(expression1);
+
+    // (): 4 * 2 = 8
+    // 5 % 2 - (8 - 1)
+    // (): 8 - 1 = 7
+    // 5 % 2 - 7
+    // %: 5 % 2 = 1
+    // 1 - 7 = -6
+
+    System.out.println(expression1);
+    
     int expression2 = (3 + (2 * 2 - 5)) + 6 - 5;
     // System.out.println(expression2);
     int expression3 = 5 * 4 % 3 - 2 + 1;
