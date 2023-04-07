b4e92af7909271bbeac026149c0d42b5e2df71c3
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-07 15:17:48 -0400

    solve GreaterThanEqualTo exercise

--- src/GreaterThanEqualTo.java
+++ src/GreaterThanEqualTo.java
@@ -3,5 +3,11 @@ public class GreaterThanEqualTo {
     double recommendedWaterIntake = 8;
     double daysInChallenge = 30;
     double yourWaterIntake = 235.5;
+
+    double totalRecommendedAmount = recommendedWaterIntake * daysInChallenge;
+    System.out.println(totalRecommendedAmount);
+
+    boolean isChallengeComplete = yourWaterIntake >= totalRecommendedAmount;
+    System.out.println(isChallengeComplete);
   }
 }
